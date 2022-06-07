1b31aed673742f2a1022eb4cc5349984d322a97e
Santosh Dahal <sdahal@mgmresorts.com>
2022-06-07 16:03:13 -0400

    Added CICD and Cypress to the App

--- cypress/integration/react.test.js
+++ cypress/integration/react.test.js
@@ -1,5 +1,6 @@
 describe('renders learn react link', () => {
     it('Should load the react landing page', () => {
         cy.visit('/')
+        cy.get('#root > div').should('contain', Landing);
     })
 });
